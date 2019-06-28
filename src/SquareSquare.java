a0107723f482059209a16491b7d7c9b10e1349d6
Maks <petrenkomaks405@gmail.com>
2019-06-28 16:40:52 +0300

    SquareSquare

---
+++ src/SquareSquare.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SquareSquare {
+    public static void main(String []args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("float = ");
+        Float number = s.nextFloat();
+        float result = number*number;
+        System.out.println("S = "+result);
+
+
+    }
+}
